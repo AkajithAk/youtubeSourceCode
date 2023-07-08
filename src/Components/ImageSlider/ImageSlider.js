b8cfc87280691a2263998d44747e62041223ddc5
AkajithAK <akajith919@gmail.com>
2023-07-08 23:18:42 +0530

    next and previous button functionality

--- src/Components/ImageSlider/ImageSlider.js
+++ src/Components/ImageSlider/ImageSlider.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './ImageSlider.css';
 function ImageSlider() {
   
@@ -8,14 +8,30 @@ function ImageSlider() {
     {id:2,value:"https://source.unsplash.com/user/c_v_r/100x100"},
   ]
   const [wordData,setWordData]=useState(imgs[0])
+  const [val,setVal] = useState(0)
   const handleClick=(index)=>{
     console.log(index)
+    setVal(index)
+    const wordSlider=imgs[index];
+    setWordData(wordSlider)
+  }
+  const handleNext = ()=>{
+    let index = val < imgs.length -1 ? val +1 : val;
+    setVal(index)
+    const wordSlider=imgs[index];
+    setWordData(wordSlider)
+  }
+  const handlePrevious = ()=>{
+    let index = val <= imgs.length -1 && val > 0? val - 1 : val;
+    setVal(index)
     const wordSlider=imgs[index];
     setWordData(wordSlider)
   }
   return (
     <div className="main">
+      <button className='btns' onClick={handlePrevious}>P</button>
       <img src={wordData.value} height="300" width="500" /> 
+      <button className='btns' onClick={handleNext}>N</button>
       <div className='flex_row'>
         {imgs.map((data,i)=>
         <div className="thumbnail" key={i} >
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import TextSlider from './Components/TextSlider/TextSlider';
 import ImageSlider from './Components/ImageSlider/ImageSlider';
+import DarkToLight from './Components/DarkToLight/DarkToLight';
+import TranslationEngToTamil from './Components/TranslationEngToTam/TranslationEngToTam';
+import Details from './Components/Details/Details';
 
-ReactDOM.render(
-    <ImageSlider />,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <ImageSlider />
 );
